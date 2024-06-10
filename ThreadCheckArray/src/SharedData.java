336cd3cbe3d907039878252b4eb6b05f16a0cca4
EldarGafarov <93073346+EldarGafarov@users.noreply.github.com>
2024-06-10 17:58:03 +0300

    Updated SharedDat.Java

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,41 +1,70 @@
+import java.util.ArrayList;
+
+/**
+ * Class containing all of the shared information between the threads.
+ */
 public class SharedData 
 {
-	private int [] array;
+	ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	private int x=5;
-	public SharedData(int[] array, int b) {
+	
+	/**
+	 * @param array
+	 * @param b
+	 * Sets B and Array parameters as the ones we got.
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * Return winArray Value
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * Set the boolean array
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	/**
+	 * return array
+	 */
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * return B value
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * return Flag Value
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * @param flag
+	 * Sets value of the shared flag to the one we got.
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
